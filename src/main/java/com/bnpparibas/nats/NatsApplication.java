4c6295c9fed6b0045659902e91329ebd6ff7605f
GOP-LINK\h.akakpo <h.akakpo@groupeonepoint.com>
2026-06-08 08:03:14 +0200

    Initial commit

---
+++ src/main/java/com/bnpparibas/nats/NatsApplication.java
@@ -0,0 +1,13 @@
+package com.bnpparibas.nats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnpparibas/nats/NatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnpparibas.nats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
